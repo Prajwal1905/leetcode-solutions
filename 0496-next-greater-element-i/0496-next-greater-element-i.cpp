c8248cebb75c61679eeb534ca7dfda58384b0b44
Prajwal Khade <150584213+Prajwal1905@users.noreply.github.com>
2026-06-23 21:34:16 +0530

    Time: 6 ms (12.46%), Space: 12.3 MB (92.4%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>ans;
+        for(int num:nums1){
+            int idx=-1;
+            for(int i=0;i<nums2.size();i++){
+                if(nums2[i]==num){
+                    idx=i;
+                    break;
+                }
+            }
+            int nextGreater=-1;
+            for(int i=idx+1;i<nums2.size();i++){
+                if(nums2[i]>num){
+                    nextGreater=nums2[i];
+                    break;
+                }
+            }
+            ans.push_back(nextGreater);
+        }
+        return ans;
+        
+    }
+};
