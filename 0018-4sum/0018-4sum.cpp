574bc9526d4b480e5085e85c614fd1ae7335fa33
Prajwal Khade <150584213+Prajwal1905@users.noreply.github.com>
2026-07-24 12:36:01 +0530

    Time: 19 ms (80.82%), Space: 17.4 MB (55.77%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -4,30 +4,33 @@ public:
         int n=nums.size();
         sort(nums.begin(),nums.end());
         vector<vector<int>>ans;
-        for(int i=0;i<n-3;i++){
-            if(i>0 && nums[i]==nums[i-1])
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]){
                 continue;
-            
-            for(int j=i+1;j<n-2;j++){
-                if(j>i+1 && nums[j]==nums[j-1])
-                    continue;
                 
-                int left=j+1;
-                int right=n-1;
-                while(left<right){
-                    long long sum=(long long)nums[i]+nums[j]+nums[left]+nums[right];
+            }
+            for(int j=i+1;j<n;j++){
+                if(j>i+1 && nums[j]==nums[j-1]){
+                    continue;
+                }
+                int l=j+1;
+                int r=n-1;
+                while(l<r){
+                    long long sum=(long long)nums[i]+nums[j]+nums[l]+nums[r];
                     if(sum==target){
-                        ans.push_back({nums[i],nums[j],nums[left],nums[right]});
-                        
-                        while(left<right && nums[left]==nums[left+1]) left++;
-                        while(left<right && nums[right]==nums[right-1]) right--;
-                        left++;
-                        right--;
-                    
+                        ans.push_back({nums[i],nums[j],nums[l],nums[r]});
+                        l++;
+                        r--;
+                        while(l<r && nums[l]==nums[l-1]){
+                            l++;
+                        };
+                        while(l<r && nums[r]==nums[r+1]){
+                            r--;
+                        }
                     }else if(sum<target){
-                        left++;
+                        l++;
                     }else{
-                        right--;
+                        r--;
                     }
                 }
             }
