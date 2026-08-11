07075a6df4bbbd25a8a937e4c4e25fa0c21a7ff6
Prajwal Khade <150584213+Prajwal1905@users.noreply.github.com>
2026-08-11 18:11:50 +0530

    Time: 305 ms (8.83%), Space: 81.5 MB (7.43%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -3,15 +3,16 @@ public:
     int lengthOfLongestSubstring(string s) {
         unordered_set<char>st;
         int left=0;
-        int ans=0;
+        int maxL=0;
         for(int right=0;right<s.length();right++){
-            while(st.count(s[right])){
+            while(st.find(s[right])!=st.end()){
                 st.erase(s[left]);
                 left++;
             }
             st.insert(s[right]);
-            ans=max(ans,right-left+1);
+            maxL=max(maxL,right-left+1);
+
         }
-        return ans;
+        return maxL;
     }
 };
